46a19fde606e1d93ded9b126433648e6eaa343f8
zz <xzrunner@gmail.com>
2022-03-04 11:26:22 +0800

    [ADDED] tiff support more formats

--- gimg_tiff.c
+++ gimg_tiff.c
@@ -5,6 +5,7 @@
 #include <tiffio.h>
 
 #include <stdlib.h>
+#include <string.h>
 
 uint8_t* 
 gimg_tiff_read_file(const char* filepath, int* width, int* height, int* format) {
@@ -14,45 +15,51 @@ gimg_tiff_read_file(const char* filepath, int* width, int* height, int* format) 
 		return NULL;
 	}
 
-	int w = 0, h = 0, bpp = 0;
+	int w = 0, h = 0, c = 0, bpp = 0;
 	TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
 	TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
+	TIFFGetField(tif, TIFFTAG_SAMPLESPERPIXEL, &c);
 	TIFFGetField(tif, TIFFTAG_BITSPERSAMPLE, &bpp);
 	bpp /= 8;
 
 	*width = w;
 	*height = h;
 
-	// fixme: only support r16 now
-	if (bpp == 2)
-	{
-		size_t size = w * h * bpp;
-		uint16_t* pixels = (uint16_t*)malloc(size);
-		if (pixels == NULL) {
-			LOGW("OOM: gimg_tiff_read_file, filepath %s, w %d, h %d", filepath, *width, *height);
-			return NULL;
-		}
-
-		size_t line_size = TIFFScanlineSize(tif);
-		tdata_t buf = _TIFFmalloc(line_size);
-		uint16_t* ptr = pixels;
-		for (int y = 0; y < h; y++) {
-			TIFFReadScanline(tif, buf, y, 0);
-			memcpy(ptr, buf, line_size);
-			ptr += w;
-		}
-
-		_TIFFfree(buf);
-		TIFFClose(tif);
-
-		*format = GPF_R16;
-
-		return (uint8_t*)pixels;
-	}
-	else
-	{
+	size_t size = w * h * c * bpp;
+	uint8_t* pixels = (uint8_t*)malloc(size);
+	if (pixels == NULL) {
+		LOGW("OOM: gimg_tiff_read_file, filepath %s, w %d, h %d", filepath, *width, *height);
 		return NULL;
 	}
+
+	size_t line_size = TIFFScanlineSize(tif);
+	tdata_t buf = _TIFFmalloc(line_size);
+	uint8_t* ptr = pixels;
+	for (int y = 0; y < h; y++) {
+		TIFFReadScanline(tif, buf, y, 0);
+		memcpy(ptr, buf, line_size);
+		ptr += line_size;
+	}
+
+	_TIFFfree(buf);
+	TIFFClose(tif);
+
+	*format = GPF_INVALID;
+	if (c == 1) {
+		if (bpp == 1) {
+			*format = GPF_RED;
+		} else if (bpp == 2) {
+			*format = GPF_R16;
+		}
+	} else if (c == 4) {
+		if (bpp == 1) {
+			*format = GPF_RGBA8;
+		} else if (bpp == 2) {
+			*format = GPF_RGBA16F;
+		}
+	}
+
+	return (uint8_t*)pixels;
 }
 
 int 
@@ -67,7 +74,31 @@ gimg_tiff_write(const char* filepath, const uint8_t* pixels, int width, int heig
 		return 0;
 	}
 
-	int bits_per_pixel = 16;
+	int channels = 0;
+	int bits_per_pixel = 0;
+	switch (format)
+	{
+	case GPF_RED:
+		channels = 1;
+		bits_per_pixel = 8;
+		break;
+	case GPF_R16:
+		channels = 1;
+		bits_per_pixel = 16;
+		break;
+	case GPF_RGBA8:
+		channels = 4;
+		bits_per_pixel = 8;
+		break;
+	case GPF_RGBA16F:
+		channels = 4;
+		bits_per_pixel = 16;
+		break;
+	default:
+		LOGW("Unknown image format %d", format);
+		return;
+	}
+
 	float refblackwhite[2 * 1];
 	refblackwhite[0] = 0.0;
 	refblackwhite[1] = (float)((1L << bits_per_pixel) - 1);
@@ -84,7 +115,7 @@ gimg_tiff_write(const char* filepath, const uint8_t* pixels, int width, int heig
 //	TIFFSetField(tif, TIFFTAG_TRANSFERFUNCTION, gray);
 	TIFFSetField(tif, TIFFTAG_RESOLUTIONUNIT, RESUNIT_NONE);
 
-	int scan_line_sz = width * (bits_per_pixel / 8);
+	int scan_line_sz = width * channels * (bits_per_pixel / 8);
 	uint8_t* ptr = pixels;
 	if (reverse) {
 		for (int i = 0; i < height; i++) {
